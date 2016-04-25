79f99419ef8563eb83ec5e73629aa3dc65653e87
takuya <takuya.1st+nospam@gmail.com>
2016-04-25 22:19:03 +0900

    chrome によるデータロック時の処理を書いた

--- lib/chrome-storage.rb
+++ lib/chrome-storage.rb
@@ -3,11 +3,13 @@
 
 require 'sqlite3'
 require 'openssl'
+require 'uri'
+require 'pry'
 
 
 module ChromeStorage
-  class << self 
-    
+  class << self
+
     @@login_db_path = File.expand_path "~/Library/Application\ Support/Google/Chrome/Default/Login\ Data"
     @@cookie_db_path = File.expand_path "~/Library/Application Support/Google/Chrome/Default/Cookies"
     @@pass = nil
@@ -15,7 +17,7 @@ module ChromeStorage
       return @@pass if @@pass
       @@pass = ` security find-generic-password -ws "Chrome Safe Storage"`.strip
     end
-    def decrypt(encrypted_value, pass ) 
+    def decrypt(encrypted_value, pass )
       iter = 1003
       key_len = 16
       salt = "saltysalt"
@@ -27,7 +29,7 @@ module ChromeStorage
 
       cipher = OpenSSL::Cipher::AES.new(128,:CBC)
       cipher.decrypt
-      cipher.iv = iv 
+      cipher.iv = iv
       cipher.key = key
 
       data = encrypted_value
@@ -35,25 +37,60 @@ module ChromeStorage
 
       return cipher.update(data) + cipher.final
     end
+    def login_data_to_keychain(*args)
+      data = dump_login_data(*args)
+      data.each{|e|
+         unless e[:username_value] && e[:password_value] then
+            puts "skipping #{[:origin_url]}"
+            next
+         end
+
+        u = URI.parse(e[:origin_url])
+        cmd = %|security add-internet-password
+          -a '#{e[:username_value]}'
+          -d #{u.host}
+          -p #{u.path}
+          -r http
+          -s #{u.host}
+          -w '#{e[:password_value]}'
+          -U
+        |.lines.map(&:strip).join(" ")
+
+        puts u.host
+
+        `#{cmd}`
+      }
+
+    end
+
     def dump_login_data(search =nil )
-      db = SQLite3::Database.new  @@login_db_path
+      db = SQLite3::Database.new  @@login_db_path , :readonly => 1
+      # binding.pry
+      ## TODO ChromeがDBロックするので、TMPにコピーして処理する必要がある。
       db.results_as_hash = true
 
       sql = "select  origin_url , username_value , password_value from logins"
       ret = []
 
-      if search then 
-        search = "%"+search+"%" 
-        sql += " where origin_url like :url" 
-        ret = db.execute(sql , {url:search})
-      else
-        ret = db.execute(sql)
+      begin
+        if search then
+          search = "%"+search+"%"
+          sql += " where origin_url like :url"
+          ret = db.execute(sql , {url:search})
+        else
+          ret = db.execute(sql)
+        end
+      rescue  SQLite3::BusyException => ex
+          puts "chrome を終了しましょう"
+          puts "Chrome 起動中は SQLite3がロックされてて、READ出来ません。"
+          exit(1)
       end
 
-      ret.map   do |e| 
+
+      ret.map   do |e|
         v = e["password_value"]
         v = self.decrypt(v,  self.chrome_pass_in_keychain )
-        e["password_value"] = v 
+        e["password_value"] = v
 
         e = {
           origin_url: e["origin_url"],
@@ -67,34 +104,33 @@ module ChromeStorage
       db = SQLite3::Database.new  @@cookie_db_path
       db.results_as_hash = true
 
-      sql = "select host_key, path, name, value, encrypted_value, secure, httponly from cookies" 
+      sql = "select host_key, path, name, value, encrypted_value, secure, httponly from cookies"
       ret = []
 
-      if search then 
-        search = "%"+search+"%" 
-        sql += " where host_key like :url" 
+      if search then
+        search = "%"+search+"%"
+        sql += " where host_key like :url"
         ret = db.execute(sql , {url:search})
       else
         ret = db.execute(sql)
       end
 
       cookie_jar ={}
-      ret.map   do |e| 
+      ret.map   do |e|
         v = e["encrypted_value"]
         v = self.decrypt(v,  self.chrome_pass_in_keychain )
-        e["encrypted_value"] = v 
+        e["decrypted_value"] = v
 
         cookie  = {
           path: e["path"],
           name: e["name"],
-          value: e["value"],
-          encrypted_value: e["encrypted_value"],
+          value: e["decrypted_value"],
           secure: e["secure"],
           httponly: e["httponly"],
         }
 
-        cookie_jar[ e["host_key"] ]  = []  unless  cookie_jar[ e["host_key"] ] 
-        cookie_jar[ e["host_key"] ] << cookie 
+        cookie_jar[ e["host_key"] ]  = []  unless  cookie_jar[ e["host_key"] ]
+        cookie_jar[ e["host_key"] ] << cookie
       end
       cookie_jar
     end
@@ -102,6 +138,3 @@ module ChromeStorage
 
   end
 end
-
-
-
